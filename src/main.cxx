2540ab7cbbe4b3577858836637e9dfb7127f5310
lune0808 <lune.taken@gmail.com>
2025-06-14 01:28:57 +0200

    move coalescing + relevant steps

--- src/main.cxx
+++ src/main.cxx
@@ -13,17 +13,17 @@ struct instr
 {
 	enum opcode_t : reg {
 		def,   // rd <- ?
-		req,   // rd -> ?
+		req,   // rd -> ? // ? <- rs1
 		copy,  // rd <- rs1
 		load,  // rd <- [rs1]
-		store, // rd -> [rs1]
-		load_local,  // rd <- local_#rs1
-		store_local, // rd -> local_#rs1
+		store, // rd -> [rs1] // TODO: [rs1] <- rs2
+		load_local,  // rd <- local_#rs1 // rd <- imm1[fp]
+		store_local, // rd -> local_#rs1 // TODO: imm1[fp] <- rs2
 		add,   // rd <- rs1 + rs2
-		imm,   // rd <- #rs1
+		imm,   // rd <- #rs1 // rd <- imm1
 	} opcode;
 	reg rd ;
-	reg rs1;
+	reg rs1; // imm1
 	reg rs2;
 };
 
@@ -59,16 +59,25 @@ struct bitset
 	using unit = std::uint32_t;
 	enum { unit_bits = sizeof(unit) * CHAR_BIT };
 	static_assert(bits <= unit_bits, "large bitfield not supported");
-	static_assert(bits != 0 && (bits & (bits-1)) == 0, "non power of 2 bitcount not supported");
 
 	bitset(unit elems);
 	T operator[](unit i) const;
 	void set(unit i, T value);
+	void acc(unit i, T value);
+	void del(unit i, T value);
 	unit popcount() const;
 
 	std::vector<unit> data;
 	unit elems;
 };
+
+template <typename T, size_t bits>
+bitset<T, bits>::bitset(unit elems)
+	: elems(elems)
+{
+	const auto units = (elems * bits + unit_bits - 1) / unit_bits;
+	data.assign(units, 0);
+}
 
 // TODO: easily vectorizable
 template <typename T, size_t bits>
@@ -82,28 +91,65 @@ bitset<T, bits> operator&(bitset<T, bits> const &lhs, bitset<T, bits> const &rhs
 	return res;
 }
 
+// TODO: easily vectorizable
 template <typename T, size_t bits>
-bitset<T, bits>::bitset(unit elems)
-	: elems(elems)
+bitset<T, bits> operator|(bitset<T, bits> const &lhs, bitset<T, bits> const &rhs)
 {
-	const auto units = (elems * bits + unit_bits - 1) / unit_bits;
-	data.assign(units, 0);
+	assert(lhs.elems == rhs.elems);
+	bitset<T, bits> res(lhs.elems);
+	for (size_t i = 0; i < lhs.data.size(); ++i) {
+		res.data[i] = lhs.data[i] | rhs.data[i];
+	}
+	return res;
 }
 
 template <typename T, size_t bits>
 T bitset<T, bits>::operator[](std::uint32_t i) const
 {
 	assert(i < elems);
-	return static_cast<T>((data[i * bits / unit_bits] >> (i * bits % unit_bits)) & ::bits(bits));
+	unit res = 0;
+	for (size_t b = 0; b < bits; ++b) {
+		size_t bit_idx = i + b * elems;
+		res |= (data[bit_idx / unit_bits] >> (bit_idx % unit_bits) & 1) << b;
+	}
+	return static_cast<T>(res);
 }
 
 template <typename T, size_t bits>
-void bitset<T, bits>::set(unit i, T value)
+void bitset<T, bits>::set(unit i, T _value)
 {
 	assert(i < elems);
-	unit msk = ::bits(bits) << (i * bits % unit_bits);
-	data[i * bits / unit_bits] &= ~msk;
-	data[i * bits / unit_bits] |=  msk & static_cast<unit>(value << (i * bits % unit_bits));
+	unit value = _value;
+	for (size_t b = 0; b < bits; ++b) {
+		size_t bit_idx = i + b * elems;
+		data[bit_idx / unit_bits] &= ~(unit{1} << (bit_idx % unit_bits));
+		data[bit_idx / unit_bits] |= (value & 1) << (bit_idx % unit_bits);
+		value >>= 1;
+	}
+}
+
+template <typename T, size_t bits>
+void bitset<T, bits>::acc(unit i, T _value)
+{
+	assert(i < elems);
+	unit value = _value;
+	for (size_t b = 0; b < bits; ++b) {
+		size_t bit_idx = i + b * elems;
+		data[bit_idx / unit_bits] |= (value & 1) << (bit_idx % unit_bits);
+		value >>= 1;
+	}
+}
+
+template <typename T, size_t bits>
+void bitset<T, bits>::del(unit i, T _value)
+{
+	assert(i < elems);
+	unit value = _value;
+	for (size_t b = 0; b < bits; ++b) {
+		size_t bit_idx = i + b * elems;
+		data[bit_idx / unit_bits] &= ~((value & 1) << (bit_idx % unit_bits));
+		value >>= 1;
+	}
 }
 
 template <typename T, size_t bits>
@@ -118,15 +164,22 @@ typename bitset<T, bits>::unit bitset<T, bits>::popcount() const
 
 struct graph
 {
-	std::vector<bitset<bool, 1>> madj;
+	enum interference { I_FREE = 0, I_NONMOVE = 1, I_MOVE = 2 /* or 3 */ };
+	static bool is_free(interference i) { return i == I_FREE; }
+	static bool is_nonmove(interference i) { return i == I_NONMOVE; }
+	static bool is_move(interference i) { return i >= I_MOVE; }
+
+	std::vector<bitset<interference, 2>> madj;
 	std::vector<reg> degree;
-	bitset<bool, 1> removed;
-	std::vector<std::pair<reg, reg>> moves;
+
+	enum state { S_VISIBLE = 0, S_FROZEN = 1, S_HIDDEN = 2 };
+	bitset<state, 2> removed;
+	std::vector<reg> move_related;
 
 	graph(reg count);
 
-	void link(reg s, reg t);
-	bool linked(reg s, reg t) const;
+	void link(reg s, reg t, interference i);
+	interference linked(reg s, reg t) const;
 	void remove(reg s);
 	bool has(reg s) const;
 	reg order() const;
@@ -134,7 +187,7 @@ struct graph
 };
 
 graph::graph(reg count)
-	: madj(count, count), degree(count), removed(count)
+	: madj(count, count), degree(count), removed(count), move_related(count)
 {
 }
 
@@ -143,30 +196,33 @@ reg graph::order() const
 	return removed.elems;
 }
 
-void graph::link(reg s, reg t)
+void graph::link(reg s, reg t, interference i)
 {
-	madj[s].set(t, true);
-	madj[t].set(s, true);
+	madj[s].acc(t, i);
+	madj[t].acc(s, i);
 	degree[s]++;
 	degree[t]++;
 }
 
-bool graph::linked(reg s, reg t) const
+graph::interference graph::linked(reg s, reg t) const
 {
 	return madj[s][t];
 }
 
 void graph::remove(reg s)
 {
-	removed.set(s, true);
+	removed.set(s, S_HIDDEN);
+	// TODO: write a unit-wise loop
 	for (reg t = 0; t < order(); ++t) {
-		degree[t] -= madj[s][t];
+		reg flag = madj[s][t];
+		flag |= flag >> 1;
+		degree[t] -= flag & 1;
 	}
 }
 
 bool graph::has(reg s) const
 {
-	return !removed[s];
+	return removed[s] == S_VISIBLE;
 }
 
 reg graph::deg(reg s) const
@@ -189,23 +245,23 @@ std::ostream &operator<<(std::ostream &os, instr const &ins)
 {
 	switch (ins.opcode) {
 	case instr::def:
-		return os << "def   %" << ins.rd;
+		return os << "def   R." << ins.rd;
 	case instr::req:
-		return os << "req   %" << ins.rd;
+		return os << "req   R." << ins.rd;
 	case instr::copy:
-		return os << "copy  %" << ins.rd << ", %" << ins.rs1;
+		return os << "copy  R." << ins.rd << ", R." << ins.rs1;
 	case instr::load:
-		return os << "load  %" << ins.rd << ", [%" << ins.rs1 << "]";
+		return os << "load  R." << ins.rd << ", [R." << ins.rs1 << "]";
 	case instr::store:
-		return os << "store %" << ins.rd << ", [%" << ins.rs1 << "]";
+		return os << "store R." << ins.rd << ", [R." << ins.rs1 << "]";
 	case instr::add:
-		return os << "add   %" << ins.rd << ", %" << ins.rs1 << ", %" << ins.rs2;
+		return os << "add   R." << ins.rd << ", R." << ins.rs1 << ", R." << ins.rs2;
 	case instr::imm:
-		return os << "imm   %" << ins.rd << ", #" << ins.rs1;
+		return os << "imm   R." << ins.rd << ", #" << ins.rs1;
 	case instr::load_local:
-		return os << "load  %" << ins.rd << ", local_" << ins.rs1;
+		return os << "load  R." << ins.rd << ", L." << ins.rs1;
 	case instr::store_local:
-		return os << "store %" << ins.rd << ", local_" << ins.rs1;
+		return os << "store R." << ins.rd << ", L." << ins.rs1;
 	default:
 		assert(false);
 	}
@@ -278,7 +334,7 @@ graph gen_graph(code_t const &code)
 	const auto idx = [&] (reg i) -> auto&   { return assert(i >= code.phys_regs), live[i - code.phys_regs]; };
 	const auto use = [&] (reg r, reg index) { if (idx(r).second < index) idx(r).second = index; };
 	const auto def = [&] (reg r, reg index) { if (idx(r).first  > index) idx(r).first  = index; };
-	const auto clobber = [&] (reg virt, reg phys) { g.link(virt, phys); };
+	const auto clobber = [&] (reg virt, reg phys) { g.link(virt, phys, graph::I_NONMOVE); };
 
 	for (reg i = 0; i < code.size(); ++i) {
 		const auto ins = code[i];
@@ -287,11 +343,13 @@ graph gen_graph(code_t const &code)
 			use(ins.rs1, i);
 			use(ins.rd , i);
 			def(ins.rd , i);
-			g.moves.emplace_back(ins.rd, ins.rs1);
+			g.link(ins.rd, ins.rs1, graph::I_MOVE);
+			++g.move_related[ins.rd ];
+			++g.move_related[ins.rs1];
 			break;
 		case instr::add:
 			use(ins.rs2, i);
-			clobber(ins.rd, 0);
+			// clobber(ins.rd, 0);
 			// fallthrough
 		case instr::load:
 			use(ins.rs1, i);
@@ -321,7 +379,7 @@ graph gen_graph(code_t const &code)
 	// render physical registers
 	for (reg t = 1; t < code.phys_regs; ++t) {
 		for (reg s = 0; s < t; ++s) {
-			g.link(s, t);
+			g.link(s, t, graph::I_NONMOVE);
 		}
 	}
 	// render virtual registers' interference
@@ -329,16 +387,15 @@ graph gen_graph(code_t const &code)
 		for (reg s = code.phys_regs; s < t; ++s) {
 			bool overlap = idx(s).first < idx(t).second && idx(t).first < idx(s).second;
 			if (overlap) {
-				g.link(s, t);
+				g.link(s, t, graph::I_NONMOVE);
 			}
 		}
 	}
 	return g;
 }
 
-stack<reg> strip(graph &interference, reg phys_regs)
+void strip(graph &interference, reg phys_regs, stack<reg> *stk, bool *stripped)
 {
-	stack<reg> stk;
 	while (true) {
 		// find any node of insignificant degree
 		// default with an impossible value
@@ -348,7 +405,9 @@ stack<reg> strip(graph &interference, reg phys_regs)
 		// it could be more sophisticated
 		reg max_deg = 0;
 		for (reg r = phys_regs; r < interference.order(); ++r) {
-			if (!interference.has(r))
+			if (interference.removed[r] == graph::S_HIDDEN
+			 || interference.removed[r] == graph::S_VISIBLE
+			  && interference.move_related[r])
 				continue;
 			const auto deg = interference.deg(r);
 			if (deg < phys_regs) {
@@ -360,11 +419,11 @@ stack<reg> strip(graph &interference, reg phys_regs)
 			}
 		}
 		if (chosen_reg == interference.order()) break;
-		stk.push(chosen_reg);
+		stk->push(chosen_reg);
 		// TODO: potential optimization : just set interference.deg(r) = virt_regs+1 so removed nodes are never selected
 		interference.remove(chosen_reg);
+		*stripped = true;
 	}
-	return stk;
 }
 
 void remap(code_t &code, std::vector<reg> const &mapping)
@@ -390,8 +449,90 @@ void remap(code_t &code, std::vector<reg> const &mapping)
 	}
 }
 
-std::vector<reg> select(graph const &interference, stack<reg> stk, reg phys_regs, bool *spilled, bitset<bool, 1> &bound)
+void merge(graph &interference, reg phys_regs, code_t &code, stack<reg> *stk, bool *merged)
 {
+	std::vector<reg> mapping(interference.order());
+	for (reg r = 0; r < mapping.size(); ++r) {
+		mapping[r] = r;
+	}
+	// Briggs test
+	for (reg s = phys_regs; s < interference.order(); ++s) {
+		if (!interference.has(s))
+			continue;
+		for (reg t = s + 1; t < interference.order(); ++t) {
+			if (!interference.has(t) || !graph::is_move(interference.linked(s, t)))
+				continue;
+			const auto neighbours = interference.madj[s] | interference.madj[t];
+			// 2-bit popcount // 00 = 0, 01/10/11 = 1
+			reg deg = 0;
+			for (size_t i = 0, bit = 0; bit < neighbours.elems * 2; ++i, bit += 32) {
+				const auto mapped = (neighbours.data[i] & 0xAAAAAAAA) >> 1 // 10/11 -> 01
+					          | (neighbours.data[i] & 0x55555555);     // 01    -> 01
+				deg += __builtin_popcount(mapped);
+			}
+			if (deg >= phys_regs)
+				continue;
+			mapping[t] = s;
+			interference.remove(t);
+			stk->push(t);
+			interference.degree[s] = deg;
+			interference.madj[s] = neighbours;
+			*merged = true;
+		}
+		interference.move_related[s] = 0;
+	}
+	for (reg s = phys_regs; s < interference.order(); ++s) {
+		if (!interference.has(s))
+			continue;
+		for (reg t = s + 1; t < interference.order(); ++t) {
+			if (!interference.has(t))
+				continue;
+			int inc = interference.linked(s, t) >> 1;
+			interference.move_related[s] += inc;
+			interference.move_related[t] += inc;
+		}
+	}
+	remap(code, mapping);
+}
+
+void freeze(graph &interference, reg phys_regs, bool *froze)
+{
+	for (reg r = phys_regs; r < interference.order(); ++r) {
+		if (!interference.has(r))
+			continue;
+		const auto deg = interference.deg(r);
+		if (deg < phys_regs) {
+			interference.removed.acc(r, graph::S_FROZEN);
+			*froze = true;
+			return;
+		}
+	}
+}
+
+void evict(graph &interference, reg phys_regs, stack<reg> *stk, bool *acted)
+{
+	reg max_deg = 0;
+	reg best = interference.order();
+	for (reg r = phys_regs; r < interference.order(); ++r) {
+		if (interference.removed[r] == graph::S_HIDDEN)
+			continue;
+		const auto deg = interference.deg(r);
+		if (deg > max_deg) {
+			max_deg = deg;
+			best = r;
+		}
+	}
+	if (best != interference.order()) {
+		stk->push(best);
+		interference.remove(best);
+		*acted = true;
+	}
+}
+
+std::vector<reg> select(graph const &interference, stack<reg> stk,
+		reg phys_regs, bool *spilled, bitset<bool, 1> &bound)
+{
+	*spilled = false;
 	auto mapping = std::vector<reg>(interference.order());
 	for (reg phys = 0; phys < phys_regs; ++phys) {
 		mapping[phys] = phys;
@@ -399,17 +540,18 @@ std::vector<reg> select(graph const &interference, stack<reg> stk, reg phys_regs
 	}
 	while (!stk.empty()) {
 		const auto s = stk.pop();
-		// represents a mask of free neighboring registers
+		// represents a mask of free registers relative to neighbors
 		reg free = bits(phys_regs);
 		assert(sizeof(free) * CHAR_BIT >= phys_regs);
-		auto is_mapped = interference.madj[s] & bound;
 		for (reg t = 0; t < interference.order(); ++t) {
 			// clears a bit corresponding to a register that is already mapped
-			free &= ~(reg(is_mapped[t]) << mapping[t]);
+			int msk = interference.madj[s][t];
+			msk |= msk >> 1;
+			free &= ~(reg(msk & bound[t]) << mapping[t]);
 		}
-		bound.set(s, !!free);
 		if (free) {
 			mapping[s] = some_bit_index(free);
+			bound.set(s, true);
 			assert(mapping[s] < CHAR_BIT * sizeof(reg));
 		} else {
 			*spilled = true;
@@ -421,13 +563,14 @@ std::vector<reg> select(graph const &interference, stack<reg> stk, reg phys_regs
 code_t rewrite(code_t const &code, bitset<bool, 1> const &bound)
 {
 	code_t next_code = { {}, code.phys_regs, code.virt_regs };
-	enum usage { use, def };
+	enum usage { use = 0, def = 1 };
 	const auto ref = [&] (reg r, usage u) {
 		assert(r >= code.phys_regs);
 		if (!bound[r]) {
 			const reg next_r = next_code.phys_regs + next_code.virt_regs++;
+			static_assert(instr::load_local + 0 == instr:: load_local);
 			static_assert(instr::load_local + 1 == instr::store_local);
-			const auto opcode = static_cast<instr::opcode_t>(instr::load_local + (u == def));
+			const auto opcode = static_cast<instr::opcode_t>(instr::load_local + static_cast<int>(u));
 			next_code.emplace_back(opcode, next_r, r);
 			r = next_r;
 		}
@@ -488,20 +631,35 @@ std::vector<reg> gcolor(code_t &code)
 	remap(code, offsets);
 	while (true) {
 		auto interference = gen_graph(code);
-		stack<reg> stk = strip(interference, code.phys_regs);
+		stack<reg> stk;
+		bool acted;
+		do {
+			do {
+				do {
+					acted = false;
+					strip(interference, code.phys_regs, &stk, &acted);
+					merge(interference, code.phys_regs, code, &stk, &acted);
+				} while (acted);
+				freeze(interference, code.phys_regs, &acted);
+			} while (acted);
+			evict(interference, code.phys_regs, &stk, &acted);
+		} while (acted);
+		assert(stk.size() >= code.virt_regs);
 		bool spilled = false;
 		bitset<bool, 1> bound(code.regs());
-		const auto mapping = select(interference, std::move(stk), code.phys_regs, &spilled, bound);
+		const auto color = select(interference, std::move(stk), code.phys_regs, &spilled, bound);
 		code = rewrite(code, bound);
 		if (!spilled) {
-			remap(code, mapping);
-			return mapping;
+			remap(code, color);
+			return color;
 		}
 	}
 }
 
 int main()
 {
+
+#if 1
 	code_t code{
 		{
 			{ instr::def , 8 },
@@ -523,10 +681,27 @@ int main()
 		4,
 		10,
 	};
-	std::cout << code;
+#endif
+#if 0
+	code_t code = {
+		{
+			{ instr::def , 0 },
+			{ instr::def , 1 },
+			{ instr::copy, 3, 0 },
+			{ instr::add , 2, 0, 1 },
+			{ instr::add , 4, 3, 1 },
+			{ instr::add , 5, 2, 4 },
+			{ instr::req , 5 },
+		},
+		3,
+		6,
+	};
+#endif
+
+	std::cout << "original:\n" << code;
 	auto m = gcolor(code);
-	std::cout << code;
-	std::cout << m;
+	std::cout << "colored:\n" << code;
+	std::cout << "coloring:\n" << m;
 	return 0;
 }
 
